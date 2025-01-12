efbab8c3a1c413c89f25c9cbc79d25ad75d98f70
shebins298 <shebins298@gmail.com>
2025-01-12 20:14:12 +0530

    Update script.js
    firebase not working

--- script.js
+++ script.js
@@ -4,11 +4,6 @@ window.onload = function() {
 };
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD6Id_0JWRmBdEMeGWPQCUxapjKf18aDek",
