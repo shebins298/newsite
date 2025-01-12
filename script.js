4d6de219f13742fdf0833b09a7023c91a98e82b3
shebins298 <shebins298@gmail.com>
2025-01-12 18:59:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,22 @@
 window.onload = function() {
     alert('Welcome to Panangad VHSS!');
 };
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD6Id_0JWRmBdEMeGWPQCUxapjKf18aDek",
+  authDomain: "newsiteproject-c00ee.firebaseapp.com",
+  projectId: "newsiteproject-c00ee",
+  storageBucket: "newsiteproject-c00ee.firebasestorage.app",
+  messagingSenderId: "440839620538",
+  appId: "1:440839620538:web:12da479da32bb5932cbf83"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
